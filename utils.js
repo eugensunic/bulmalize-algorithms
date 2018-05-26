a085a4eeaf2c9cea7fbdc4c5ea834ca47d64345e
Eugen Sunic <eugen.sunic@extern.consorsbank.de>
2018-05-26 09:42:30 +0200

    2.commit

--- utils.js
+++ utils.js
@@ -1,9 +1,12 @@
 (function() {
   let colorArray = ['#ff3860', '#3273dc', '#00d1b2', '#ffdd57', '#23d160', '#e8e8e8'];
+
+  // change background color on hover
   for (let i = 0; i < document.querySelectorAll('.box-config').length; i++) {
     document.querySelectorAll('.box-config')[i].addEventListener('mouseover', function() {
       document.querySelectorAll('.box-config')[i].style.background = colorArray[getRandomNum(0, colorArray.length - 1)];
     });
+
     // clear background color on mouseout
     document.querySelectorAll('.box-config')[i].addEventListener('mouseout', function() {
       document.querySelectorAll('.box-config')[i].style.background = 'white';
